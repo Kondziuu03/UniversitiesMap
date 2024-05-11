7b09e863389b2169d8f5c640ee5f46b1e07608b4
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2024-05-11 11:26:19 +0200

    Added mockup if request fails

--- UniversitiesFront/src/App.tsx
+++ UniversitiesFront/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import React from "react";
 
-import UNIVERSITIES from "../mock/universities";
+import UNIVERSITIES, { University } from "../mock/universities";
 import AddForm from "./AddForm";
 import List from "./List";
 import GoogleMap from "./Map";
@@ -13,13 +13,25 @@ function App() {
   const [text, setText] = React.useState("");
   const [universities, setUniversities] = React.useState([]);
   const filteredUniversities = React.useMemo(() => {
-    return UNIVERSITIES.filter(
+    return universities.filter(
       // universities
-      (university) =>
+      (university: University) =>
         university.name.toLowerCase().includes(text.toLowerCase()) ||
         university.description.toLowerCase().includes(text.toLowerCase())
     );
   }, [text, universities]);
+  React.useEffect(() => {
+    getUniversities()
+      .then((res) => {
+        const data = res.data;
+        setUniversities(data);
+      })
+      .catch((e) => {
+        console.error("Error", e);
+        setUniversities(UNIVERSITIES);
+      });
+  });
+
   const [location, setLocation] = React.useState({
     x: undefined,
     y: undefined,
