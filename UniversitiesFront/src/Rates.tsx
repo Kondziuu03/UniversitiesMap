6666d7ea8667b86bf9c007278918769f201b7bc0
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2024-06-08 16:58:49 +0200

    Added average rate

--- UniversitiesFront/src/Rates.tsx
+++ UniversitiesFront/src/Rates.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Rate } from "./service";
 
 interface RatesProps {
@@ -6,10 +7,14 @@ interface RatesProps {
 
 export default function Rates(props: RatesProps) {
   const { rates } = props;
+  const avg = React.useMemo(() => {
+    const sum = rates.reduce((acc, rate) => acc + rate.rateValue, 0);
+    return sum / rates.length;
+  }, [rates]);
 
   return (
     <div>
-      <h3>Rates</h3>
+      <h3>Rates - {avg}</h3>
       {rates.length === 0 ? (
         <span>No rates yet</span>
       ) : (
